9373a1755f395e416c89b4352c527d53a91132c9
Bepnesh Golcharan <alexsamm231@hotmail.com>
2017-07-29 11:06:35 -0400

    Add files via upload

---
+++ enc.py
@@ -0,0 +1,44 @@
+# Name: Enc.py (Encrytion script)
+# Author: uncle_beppy
+# Version: v0.1 (beta)
+# Description:
+# Encryption program based on ROT13
+
+#!/usr/bin/python
+
+import sys
+
+message = ""
+
+if len(sys.argv)<=1:
+    print("Usage: encrypt.py <message>")
+    sys.exit(0)
+else:
+    message = sys.argv[1]
+
+def rot13(s):
+    result = ""
+
+    # Loop through characters
+    for v in s:
+        # Convert to number
+        c = ord(v)
+
+        if c >= ord('a') and c <= ord('z'):
+            if c > ord('m'):
+                c -= 13
+            else:
+                c += 13
+        elif c >= ord('A') and c <= ord('Z'):
+            if c > ord('M'):
+                c -= 13
+            else:
+                c += 13
+
+        result += chr(c)
+
+    return result
+
+# Test method
+print(rot13(message))
+#print(rot13(rot13(message)))
